65866a7d783d6736848ec175bc5550830e8c1754
Arieta Haskaj <arietah8545@hstat.org>
2018-10-10 02:21:53 +0000

    pass rspec tests

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,23 @@
-def division
+def division(num1, num2)
+    num1/num2
+end
+
+def argue(input)
+  input
+end
+
+def greeting(greeting, name)
     
-end
-
-def argue
-  
-end
-
-def greeting
-
 end
 
 def return_a_value
-    
+    "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(topping="cheese")
+    topping
 end
 
-def whisper
-    
+def whisper(word)
+    word.downcase
 end
